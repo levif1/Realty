9da374516bd59a7613c26b6175618305a6fbcbec
jwaugh3 <jwaugh3@gmail.com>
2019-08-23 16:46:30 -0500

    Listing Pages, contact page, and login
    front end completed for these pages

--- register/register.php
+++ register/register.php
@@ -8,10 +8,10 @@ include("../includes/nav.php");
 
 
 <div class="container">
-    <div class="row">
+    <div class="row loginPadding">
       <div class="col-lg-10 col-xl-9 mx-auto">
         <div class="card card-signin flex-row my-5">
-          
+
           <div class="card-body">
             <h5 class="card-title text-center">Register</h5>
             <form class="form-signin" action = "register.inc.php" method="POST">
@@ -21,24 +21,26 @@ include("../includes/nav.php");
                 <input type="email" id="inputEmail" name = "inputEmail" class="form-control" placeholder="Email address" required>
                 <label for="inputEmail">Email address</label>
               </div>
-              
+
               <hr>
 
               <div class="form-label-group">
                 <input type="password" id="inputPassword" name = "inputPassword" class="form-control" placeholder="Password" required>
                 <label for="inputPassword">Password</label>
               </div>
-              
+
               <div class="form-label-group">
                 <input type="password" id="inputConfirmPassword" name = "inputConfirmPassword" class="form-control" placeholder="Password" required>
                 <label for="inputConfirmPassword">Confirm password</label>
               </div>
 
-              <button class="btn btn-lg btn-primary btn-block text-uppercase" type="submit" name="signup-submit">Register</button>
+              <button class="btn-register btn-lg" type="submit" name="signup-submit">Register</button>
               <a class="d-block text-center mt-2 small" href="../login/login.php">Sign In</a>
               <hr class="my-4">
-              <button class="btn btn-lg btn-google btn-block text-uppercase" type="submit"><i class="fab fa-google mr-2"></i> Sign up with Google</button>
-              <button class="btn btn-lg btn-facebook btn-block text-uppercase" type="submit"><i class="fab fa-facebook-f mr-2"></i> Sign up with Facebook</button>
+              <div class="vertAlignButton container">
+              <button class="row btn-outline-danger btn-google" type="submit"><i class="fab fa-google mr-2"></i> Sign up with Google</button>
+              <button class="row btn-outline-primary btn-facebook" type="submit"><i class="fab fa-facebook-f mr-2"></i> Sign up with Facebook</button>
+            </div>
             </form>
           </div>
         </div>
